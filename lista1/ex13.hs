2f0128e11c9a83c30ee2f017284f3be056bb68f5
piotto <viniciuspiotto.dev@gmail.com>
2024-04-08 21:39:47 -0300

    Revert "ex 13"
    This reverts commit abbaa762a943ed3f238d621fe731b8e6acbaa4a6.

--- lista1/ex13.hs
+++
@@ -1,8 +0,0 @@
-howManyEqual :: Int -> Int -> Int -> Int
-howManyEqual x y z
-    | isEqual x y && isEqual y z = 3
-    | isEqual x y || isEqual y z || isEqual x z = 2
-    | otherwise = 0
-
-isEqual :: Int -> Int -> Bool
-isEqual x y = x == y 
